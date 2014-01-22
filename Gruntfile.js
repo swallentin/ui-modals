4241d84c793a531df651ba7bb7bb1f51d4631568
Stephan Wallentin <stephan.wallentin@gmail.com>
2014-01-22 13:34:13 +0100

    Added simple grunt connect server and livereload stuff.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,29 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+        clean: ['build/'],
+        copy: {
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: '**',
+                    dest: 'build/'
+                }, {
+                    expand: true,
+                    src: ['bower_components/**/*.js', 'bower_components/**/*.css'],
+                    dest: 'build/'
+                }]
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    base: 'build/',
+                }
+            }
+        },
         karma: {
             unit: {
                 configFile: 'config/karma.conf.js',
@@ -8,15 +31,26 @@ module.exports = function(grunt) {
             }
         },
         watch: {
+            options: {
+                livereload: true                
+            },
             karma: {
                 files: ['src/**/*.js', 'test/unit/**/*.js'],
                 tasks: ['karma:unit:run']
+            },
+            app: {
+                files: ['src/**/*'],
+                tasks: ['copy']
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-karma');
 
     grunt.registerTask('devmode', ['karma:unit', 'watch']);
+    grunt.registerTask('server', ['clean', 'copy', 'connect', 'watch:app']);
 };
